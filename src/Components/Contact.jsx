8a44aa9b6e41057a6675493e4eecdb62440fc2ab
ShailajaTripathi <shailaja24305@gmail.com>
2022-03-31 19:40:27 +0530

    completed all components

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./contact.css";
-
+import Icon from './Icon'
 function Contact() {
   return (
     <div className="contact-container" id="contact">
@@ -11,58 +11,66 @@ function Contact() {
       </div>
       <div className="contact-details">
         <div className="form-container">
-
-            <form>
-                <div className="form-flex">
-                <div   style={{ flex: "0 0 auto",
-    width: "45%"}}>     
-            <label for="fname">First Name</label>
-            <input
-              type="text"
-              id="fname"
-              name="firstname"
-            />
+          <form>
+            <div className="form-flex">
+              <div style={{ flex: "0 0 auto", width: "45%" }}>
+                <label for="fname">First Name</label>
+                <input type="text" id="fname" name="firstname" />
+              </div>
+              <div style={{ flex: "0 0 auto", width: "45%" }}>
+                <label for="lname">Last Name</label>
+                <input type="text" id="lname" name="lastname" />
+              </div>
             </div>
-            <div   style={{ flex: "0 0 auto",
-    width: "45%"}}>
-             <label for="lname">Last Name</label>
-            <input
-              type="text"
-              id="lname"
-              name="lastname"
-            />
-            </div></div>
-         
-            <label for="lname" >Email Address</label>
+
+            <label for="lname">Email Address</label>
             <input
               type="text"
               id="email"
               name="email"
-              style={{ width: "100%",textAlign: "left" }}
+              style={{ width: "100%", textAlign: "left" }}
             />
-            
+
             <label for="message">Message</label>
             <textarea
               id="message"
               name="message"
-              style={{height:"200px"}}
+              style={{ height: "200px" }}
             ></textarea>
-<a href="#" className="btn-details" style={{width:"120px",padding:"3%",fontSize:"15px"}}>Send Message</a>
-            </form>
-         
+            <a
+              href="#"
+              className="btn-details"
+              style={{ width: "120px", padding: "3%", fontSize: "15px" }}
+            >
+              Send Message
+            </a>
+          </form>
         </div>
         {/* <div className="social-media-icons">
    
         </div> */}
 
         <div className="contact-data">
-<p>155 Market St #101, Paterson, NJ<br/> 07505, United States</p>
-<p>+1 202 2020 200</p>
-<p>@info@mydomain.com</p>
+          <p>
+           
+            <i className="fas fa-home"></i>
+            <span className="icon-span"></span> 155 Market St #101,Paterson,NJ 07505, United States
+          </p>
+          <p>
+            <i class="fas fa-phone"></i>
+            <span className="icon-span"></span> +1 202 2020 200
+          </p>
+          <p>
+            <i class="fas fa-envelope"></i>
+            <span className="icon-span"></span>@info@mydomain.com
+          </p>
         </div>
+      
       </div>
+      <Icon/>
     </div>
   );
 }
 
 export default Contact;
+
---
+++ src/Components/Icon.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './icon.css'
+function Icon() {
+  return (
+    <div className="icon-flex">
+      {/* <i class="fas fa-instagram"></i>
+      <i class="fas fa-twitter"></i>
+      <i class="fas fa-facebook"></i>
+<i class="fa-brands fa-linkedin"></i> */}
+
+    </div>
+  )
+}
+
+export default Icon
